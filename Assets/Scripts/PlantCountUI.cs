2b2b06363739a598137850eb401949c1d55515f3
tinamengxq <tinam5@uci.edu>
2026-01-08 20:12:46 -0800

    1/8

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -6,8 +6,11 @@ public class PlantCountUI : MonoBehaviour
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
+
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        _remainingText.text = $"{seedsLeft}";
+        _plantedText.text = $"{seedsPlanted}";
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,36 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft=_numSeeds;
+        _numSeedsPlanted=0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft,_numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        MoveAround();
+        PlantSeed();
+    }
+
+    void MoveAround()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 vector3 = new Vector3(horizontal,vertical,0f);
+        _playerTransform.position += vector3*_speed*Time.deltaTime;
     }
 
     public void PlantSeed ()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_numSeedsLeft > 0)
+            {
+                Instantiate(_plantPrefab, _playerTransform.position, _playerTransform.rotation);
+                _numSeedsLeft--;
+                _numSeedsPlanted++;
+                _plantCountUI.UpdateSeeds(_numSeedsLeft,_numSeedsPlanted);
+            }
+        }
     }
 }
